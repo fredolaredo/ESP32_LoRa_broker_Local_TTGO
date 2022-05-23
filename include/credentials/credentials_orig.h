c2359508e5d9da6c97591c2dcd8d2347e063af29
fredolaredo <frederic.benzaquine@free.fr>
2022-05-23 16:19:33 +0200

    init_2

---
+++ include/credentials/credentials_orig.h
@@ -0,0 +1,13 @@
+
+#define MQTT_SERVER "192.168.xx.xx"
+#define MQTT_PORT   xxxx
+#define MQTT_LOGIN  "xxxxx"
+#define MQTT_PASSWD "xxxxxxxxxxxxxxxx"
+
+
+#define AP_NAME       "xxxxx"
+#define AP_PASSRHRASE "xxxxxxxxxxxxxxxxxxxxxxxxx"
+
+
+
+
